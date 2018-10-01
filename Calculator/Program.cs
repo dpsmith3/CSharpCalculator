dedae452f7fb66cafe6cadd2d7a84002bd440d8d
Dan Smith <Daniel.Smith@softwire.com>
2018-10-01 14:34:08 +0100

    Added a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,6 +11,12 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("Please enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number");
+            int num2 = int.Parse(Console.ReadLine());
+            int result = num1 * num2;
+            Console.WriteLine("The product of those two numbers is: {0}", result);
             Console.ReadLine();
         }
     }
