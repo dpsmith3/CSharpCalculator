9b11b27a03fe88d3d8908078af6883da295f4425
Dan Smith <Daniel.Smith@softwire.com>
2018-10-01 16:57:59 +0100

    For loops and arrays

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,12 +11,57 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
-            Console.WriteLine("Please enter a number");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter another number");
-            int num2 = int.Parse(Console.ReadLine());
-            int result = num1 * num2;
-            Console.WriteLine("The product of those two numbers is: {0}", result);
+
+            Console.WriteLine("Please enter the operator: ");
+            string operator1 = Console.ReadLine();
+
+            Console.WriteLine("How many numbers to do you want to {0}", operator1);
+            int iterations = int.Parse(Console.ReadLine());
+
+            float[] numbers = new float[iterations];
+            for (int i = 0; i < iterations; i++)
+            {
+                Console.WriteLine("Please enter number {0}:", i + 1);
+                numbers[i] = float.Parse(Console.ReadLine());
+            }
+
+            if (operator1 == "+")
+            {
+                double result = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    result += numbers[i];
+                }
+                Console.WriteLine("The result is {0}", result);
+            } else if (operator1 == "-") {
+                double result = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    result -= numbers[i];
+                }
+                Console.WriteLine("The result is {0}", result);
+            } else if (operator1 == "*")
+            {
+                double result = 1;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    result *= numbers[i];
+                }
+                Console.WriteLine("The result is {0}", result);
+            } else if (operator1 == "/")
+            {
+                double result = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    result /= numbers[i];
+                }
+                Console.WriteLine("The result is {0}", result);
+            } else
+            {
+                Console.WriteLine("The operator you entered is not valid");
+
+            }
+
             Console.ReadLine();
         }
     }
